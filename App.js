c2dd9c67233a033c26c8b3b9d16e4a6934fb43f1
Pedro3g <54337976+Pedroor@users.noreply.github.com>
2021-01-07 11:44:18 -0300

    Configurate project and check the example in App.js

--- App.js
+++ App.js
@@ -1,14 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState, Platform } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import { Camera } from 'expo-camera';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [permission, setPermission] = useState(null);
+
+  useEffect(() => {
+    permissionFlow();
+  }, []);
+
+  const permissionFlow = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    console.log(status);
+    setPermission(status === 'granted');
+  };
+
+  if (permission !== 'granted') {
+    return (
+      <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Funciona</Text>
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style='auto' />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,6 +39,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
--- README.md
+++
@@ -1,2 +0,0 @@
-# request-permissions-in-expo-cli
- testing permissions in expo to resolve default messages in ios bundle.
